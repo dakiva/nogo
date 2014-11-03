30db951af4181102e7aeadff3581ebc9cd60a9c1
Daniel Akiva <dakiva@gmail.com>
2014-11-02 20:35:47 -0800

    Added indexes to secure resource table. Minor naming changes. Removed bad FK reference.

--- schema/postgres/nogo_ddl.sql
+++ schema/postgres/nogo_ddl.sql
@@ -16,22 +16,29 @@ CREATE INDEX ix_role_member_principal_sid ON role_member (
 );
 
 CREATE TABLE secure_resource (
-       resource_id           bigserial PRIMARY KEY,
-       native_resource_id    text UNIQUE NOT NULL,
-       parent_resource_id    bigint,
-       owner_sid             text,
-       inherit_parent_acl    boolean NOT NULL,
-       CONSTRAINT fk_secure_resource_parent_resource_id FOREIGN KEY(parent_resource_id) REFERENCES secure_resource(resource_id),
-       CONSTRAINT fk_secure_resource_owner_id FOREIGN KEY(owner_id) REFERENCES sid(sid_id)
+       secure_resource_id        bigserial PRIMARY KEY,
+       native_resource_id        text UNIQUE NOT NULL,
+       parent_secure_resource_id bigint,
+       owner_sid                 text,
+       inherit_parent_acl        boolean NOT NULL DEFAULT true,
+       CONSTRAINT fk_secure_resource_parent_secure_resource_id FOREIGN KEY(parent_resource_id) REFERENCES secure_resource(secure_resource_id)
+);
+
+CREATE INDEX ix_secure_resource_native_resource_id ON secure_resource (
+       native_resource_id
+);
+
+CREATE INDEX ix_secure_resource_owner_sid ON secure_resource (
+       owner_sid
 );
 
 CREATE TABLE acl_entry (
-       id              bigserial PRIMARY KEY,
-       resource_id     bigint NOT NULL,
-       principal_sid   text NOT NULL,
-       permission_mask int NOT NULL,
-       CONSTRAINT ix_acl_entry_acl_resource_id_sid UNIQUE(resource_id, principal_sid),
-       CONSTRAINT fk_acl_entry_acl_resource_id FOREIGN KEY(resource_id) REFERENCES secure_resource(resource_id) ON DELETE CASCADE
+       id                 bigserial PRIMARY KEY,
+       secure_resource_id bigint NOT NULL,
+       principal_sid      text NOT NULL,
+       permission_mask    int NOT NULL,
+       CONSTRAINT ix_acl_entry_acl_secure_resource_id_sid UNIQUE(secure_resource_id, principal_sid),
+       CONSTRAINT fk_acl_entry_acl_secure_resource_id FOREIGN KEY(secure_resource_id) REFERENCES secure_resource(secure_resource_id) ON DELETE CASCADE
 );
 
 CREATE INDEX ix_acl_entry_principal_sid ON acl_entry (
